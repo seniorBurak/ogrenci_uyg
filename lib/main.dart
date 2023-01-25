317b56ea66fbab7cffbf0dad3d9e2241244a5970
makina <makina@DESKTOP-R39P48C>
2023-01-26 00:45:20 +0300

    google firebaseStorage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ogrenci_uyg/pages/mesajlar_sayfasi.dart';
 import 'package:ogrenci_uyg/pages/ogrenciler_sayfasi.dart';
 import 'package:ogrenci_uyg/pages/ogretmenler_sayfasi.dart';
@@ -135,12 +140,7 @@ class AnaSayfa extends ConsumerWidget {
       drawer: Drawer(
         child: ListView(
           children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(FirebaseAuth.instance.currentUser!.displayName!),
-            ),
+            const UserHeader(),
             ListTile(
               title: const Text('Öğrenciler'),
               onTap: () {
@@ -198,5 +198,95 @@ class AnaSayfa extends ConsumerWidget {
         return const MesajlarSayfasi();
       },
     ));
+  }
+}
+
+class UserHeader extends StatefulWidget {
+  const UserHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<UserHeader> createState() => _UserHeaderState();
+}
+
+class _UserHeaderState extends State<UserHeader> {
+  Future<Uint8List?>? _ppicFuture;
+  @override
+  void initState() {
+    super.initState();
+    _ppicFuture = _ppicIndir();
+  }
+
+  Future<Uint8List?> _ppicIndir() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    final documentSnapshot = await FirebaseFirestore.instance
+        .collection('kullanıcılar')
+        .doc(uid)
+        .get();
+
+    final userRecMap = documentSnapshot.data();
+    if (userRecMap == null) return null;
+
+    if (userRecMap.containsKey('ppicref')) {
+      Uint8List? uint8list =
+          await FirebaseStorage.instance.ref(userRecMap['ppicref']).getData();
+      return uint8list;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(FirebaseAuth.instance.currentUser!.displayName!),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            onTap: () async {
+              XFile? xFile =
+                  await ImagePicker().pickImage(source: ImageSource.camera);
+              if (xFile == null) return;
+
+              final imagePath = xFile.path;
+
+              final uid = FirebaseAuth.instance.currentUser!.uid;
+
+              final ppicRef =
+                  FirebaseStorage.instance.ref('ppics').child('$uid.jpg');
+              await ppicRef.putFile(File(imagePath));
+
+              await FirebaseFirestore.instance
+                  .collection('kullanıcılar')
+                  .doc(uid)
+                  .update({'ppicref': ppicRef.fullPath});
+
+              setState(() {
+                _ppicFuture = _ppicIndir();
+              });
+            },
+            child: FutureBuilder<Uint8List?>(
+                future: _ppicFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    final picInMemory = snapshot.data!;
+                    return CircleAvatar(
+                      backgroundImage: MemoryImage(picInMemory),
+                    );
+                  }
+                  return const CircleAvatar(
+                    child: Text('BD'),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
